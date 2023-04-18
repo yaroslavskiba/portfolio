d088a22d338e075d420cd710543b189360ff78ff
yaroslav skiba <109871666+yaroslavskiba@users.noreply.github.com>
2023-04-18 22:36:35 +0300

    upd

--- src/components/content/main.tsx
+++ src/components/content/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { animated, useTransition } from 'react-spring';
 import About from './about';
 import Blog from './blog';
 import Contact from './contact';
@@ -8,16 +9,29 @@ import Projects from './projects';
 import Skills from './skills';
 
 const Main = () => {
+  const location = useLocation();
+
+  const transitions = useTransition(location, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: null,
+    config: { duration: 1000 },
+  });
+
   return (
     <main className="main">
-      <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/blog" element={<Blog />} />
-      </Routes>
+      {transitions((props, item) => (
+        <animated.div style={props}>
+          <Routes location={item}>
+            <Route path="/" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
+        </animated.div>
+      ))}
     </main>
   );
 };
