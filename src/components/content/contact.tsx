5f0e0766e3af278b704e959dc1854c47a22fc5bd
yaroslav skiba <109871666+yaroslavskiba@users.noreply.github.com>
2023-04-26 20:15:00 +0300

    upd

--- src/components/content/contact.tsx
+++ src/components/content/contact.tsx
@@ -1,5 +1,98 @@
 import React from 'react';
+import { SiInstagram, SiFuturelearn } from 'react-icons/si';
+import { TbBrandTelegram } from 'react-icons/tb';
+import { RxTwitterLogo } from 'react-icons/rx';
+import { GoMail } from 'react-icons/go';
+import { VscGithub } from 'react-icons/vsc';
+import { FaFreeCodeCamp } from 'react-icons/fa';
+import { BsTelephoneForward } from 'react-icons/bs';
 
-const Contact = () => <h2>Contact</h2>;
+interface ContactItem {
+  name: string;
+  icon: React.ReactNode;
+  link: string;
+}
+
+const contactItems: ContactItem[] = [
+  {
+    name: 'Instagram',
+    icon: <SiInstagram />,
+    link: 'https://www.instagram.com/',
+  },
+  {
+    name: 'Telegram',
+    icon: <TbBrandTelegram />,
+    link: 'https://telegram.org/',
+  },
+  {
+    name: 'Twitter',
+    icon: <RxTwitterLogo />,
+    link: 'https://twitter.com/',
+  },
+  {
+    name: 'Gmail',
+    icon: <GoMail />,
+    link: 'https://mail.google.com/',
+  },
+  {
+    name: 'GitHub',
+    icon: <VscGithub />,
+    link: 'https://github.com/',
+  },
+  {
+    name: 'Hexlet',
+    icon: <SiFuturelearn />,
+    link: 'https://hexlet.io/',
+  },
+  {
+    name: 'FreeCodeCamp',
+    icon: <FaFreeCodeCamp />,
+    link: 'https://freecodecamp.com/',
+  },
+];
+
+const Contact: React.FC = () => {
+  return (
+    <div className="contact-container">
+      <h1 className="title">
+        Contact <span className="color-main">Me</span>
+      </h1>
+      <div className="contact-info">
+        <p className="contact-text">
+          <span className="color-main">I</span> use <span className="color-main">social media</span> to keep up with the{' '}
+          <span className="color-main">latest news and trends</span> in my areas of interest, but I&apos;m not a big
+          poster. I check in <span className="color-main">regularly</span> but don&apos;t spend a lot of time scrolling
+          through my feeds. However, I&apos;m always available and responsive to{' '}
+          <span className="color-main">messages and calls</span>, so don&apos;t hesitate to contact me if you want to
+          get in touch!
+        </p>
+        <p>
+          <span className="color-main">My</span>links:
+        </p>
+        <ul className="contact-list">
+          {contactItems.map((item) => (
+            <li key={item.name}>
+              <a href={item.link} target="blank" className="sc">
+                {item.name}
+                {item.icon}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <p className="contact-text">
+          <span className="color-main">Hey</span>, feel free to give me a call using the{' '}
+          <span className="color-main">link below</span>! Wishing you an awesome day and a fantastic mood!{' '}
+          <span className="color-main">Thanks for taking the time to check out my portfolio</span>.
+        </p>
+        <a href="tel:+375295464591" className="tel-link">
+          <span className="pulsed-link">
+            <BsTelephoneForward />
+          </span>
+          Call <span className="color-main">Me</span>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
